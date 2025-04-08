30eb013fbf895d7f5234d3198ab32af0f2da2794
EDsLi1103 <44909161+EDsLi1103@users.noreply.github.com>
2025-04-08 18:31:52 +0300

    Перевод надписей

--- SS14.Launcher/Marseyverse/TitleManager.cs
+++ SS14.Launcher/Marseyverse/TitleManager.cs
@@ -17,7 +17,7 @@ public class TitleCondition
 
 public class TitleManager
 {
-    private const string BanMessage = "Banned";
+    private const string BanMessage = "Забанен";
     private static readonly Random Random = new Random();
 
     public TitleManager()
@@ -56,73 +56,78 @@ public class TitleManager
 
     private static readonly List<string> Titles =
     [
-        "Space Station 14 Launcher", "Dramalauncher",
-        "Marsey", "Moonyware", "Marseyloader",
-        "Robustcontrol", "Mirailoader", "Almost BepInEx",
-        "Video game launcher", "MIT-certified funny",
-        "戏剧装载机", "Oldest anarchy launcher in ss14",
+        "Лаунчер Space Station 14", "Драмалаунчер",
+        "Марси", "Мунливэйр", "Марсилоадер",
+        "Робастконтроль", "Мирайлоадер", "Почти BepInEx",
+        "Лаунчер для видеоигр", "Сертифицировано MIT как смешное",
+        "ЭДсЛи пидор ебаный", "ЭДсЛи иди нахуй", "Старейший анархичный лаунчер в SS14",
         "ILVerifier", "Space Station 13", "BYOND",
-        "Goonstation", "BYONDCONTROL", "Unitystation",
-        "Stationeers", "RE:SS2D", "Schizostation 14",
-        "Thaumatrauma", "Calamari v3", "hamaSS14", BanMessage
+        "Гунстэйшн", "BYONDCONTROL", "Юнитистэйшн",
+        "Стейшнерс", "RE:SS2D", "Шизостэйшн 14",
+        "Таумотравма", "Каламати v3",
+        "Шпионское ПО Синдиката (Легальная Версия)", "БВОИНК Лаунчер",
+        "Робастификатор 9000", "Симулятор Каргонии", "Макскап Эксплозив 14",
+        "BYOND, но лучше", "ЯщероЛоадер 14", "Рай Шизопостеров",
+        "Грифстэйшн 14",
+        "Фирменный лаунчер НаноТрейзен (Теперь на 200% робастнее)", BanMessage
     ];
 
     private static readonly List<string> TagLines =
     [
-        "Marsey is the cutest cat", "Not a cheat loader",
-        "PR self-merging solutions", "RSHOE please come back",
-        "Today I learned RSHOE is datae, it has never been more over",
-        "As audited on discord.gg/ss14", "Leading disabler of engine signature checks",
-        "Sigmund Goldberg died for this", "Sandbox sidestep simulator", "DIY bomb tutorials",
-        "incel matchmaking service", "#1 ransomware provider", "God, King & Bussy",
-        "The mayocide is coming", "Leading forum for misandrists", "Trans Rights!",
-        "Largest long bacon provider", "天安門大屠殺", "Shitcode tester",
-        "The code for the client literally calls itself a cheat client",
-        "Tumblr client for fujoshis", "Cheap airfare and hotels", "Aliyah consulting",
-        "Friday Night Funkin mod manager", "download .apk MOD (Infinite money, health, free admin)",
-        "Disaster generator", "Primary cause of binary blob discussions",
-        "George Bush did 9/11 and yet I'm the bad guy", "Hybristophiliac support group",
-        "Space game but awesome", "Game SUCKS I go bed", "Go be ironic on wizden",
-        "On payroll from the saudi government", "Handling the cheating situation since November",
-        "World-class anti-cheat", "Leading cause of secondary psychosis",
-        "they even made one for fastnoiselite which tells me some people have brain damage",
-        "Station Announcement: Buttfucker69 (Medical Intern) has arrived at the station!",
-        "Game's not out but there's 7,000+ ban appeals"
+        "Марси — самый милый котик", "Не читерский лоадер",
+        "Решения для самовольного мержа PR", "RSHOE пожалуйста вернись",
+        "Сегодня я узнал что RSHOE — это дате, теперь всё кончено",
+        "Как подтверждено на discord.gg/ss14", "Ведущий отключитель проверок сигнатур движка",
+        "Зигмунд Голдберг умер за это", "Симулятор обхода сандбокса", "Гайды по созданию бомб своими руками",
+        "Сервис знакомств для инцелов", "Первый поставщик ransomware-программ", "Читер",
+        "Маёцид приближается", "Главный форум мисандристов", "Права трансам! На сожжение в крематории.",
+        "Крупнейший поставщик больших шарфов", "ЭДсЛи ты пидор", "Тестер говнокода",
+        "Клиентский код буквально называет себя читерским клиентом",
+        "Тумблр-клиент для фуджоши", "Дешёвые авиабилеты и отели", "Консалтинг по алии",
+        "Менеджер модов для Friday Night Funkin", "Скачать .apk MOD (Бесконечные деньги, здоровье, бесплатный админ)",
+        "Генератор катастроф", "Основная причина споров о бинарных блобах",
+        "Джордж Буш сделал 9/11, а я почему-то плохой парень", "Группа поддержки гибристофиликов",
+        "Космическая игра, но крутая", "Игра ОТСТОЙ, я спать", "Иди иронизируй на wizden",
+        "На зарплате у саудовского правительства", "Разбираюсь с читерами с ноября",
+        "Античит мирового класса", "Основная причина вторичного психоза",
+        "они даже сделали это для fastnoiselite, что говорит мне о повреждении мозга у некоторых людей",
+        "Объявление на станции: Buttfucker69 (Мед. интерн) прибыл на станцию!",
+        "Игра ещё не вышла, но уже 7000+ банаппилов"
     ];
 
     // Have I truly lost any imagination? Yeah, kind of.
     private static readonly List<string> BanReasons =
     [
-        "Using a modified client", "Self-antag", "Ban evasion", "Datacenter",
-        "Mirrored ban from nyano", "Vac banned from secure server", "Unpleasant to deal with on github and ingame",
-        "As CMO participated in slave RP", "Left seconds after receiving an ahelp.", "Claiming to be hitler of jamaican jazz",
-        "eat shit you sick fuck", "GRUGSTEIN", "alt", "welderbombed a group of sec", "took a welding tank for a walk",
-        "raider", "mass murdered people with the gib stick from a present", "overall self-antag and grief of the security department",
-        "Gifted a chicken mob to warden, calling it \"BBC\"", "Cult. Appeal in 6 months.", "Literally has 20+ accounts.",
-        "liked a transphobic post on twitter", "RP'ing as jocks beating up (killing) nerds (5) and shoving them into lockers",
-        "Character named \"ADOLF RIZZLER\"", "wrote on a piece of paper \"I pissed myself\"", "Gross imcompetence as captain",
-        "Giving out aa ids", "Being swept", "Being karma", "teaching people how to make schedule 1 narcotics", "Hitler RP in cargo orders",
-        "Selling the nuke", "Actively trying to build up a room of miasma", "Set AME to 140 then Disconnected",
-        "Exploiting. Teleported into armory using chairs.", "rule 0 permaban speedrun world champion ", "Take a break, bud",
-        "No-appeal perma ban based on advice of the player and other hosts", "Metacomms", "\"soft antag rolling,\" powergaming, being rude in OOC",
-        "Warned multiple times about powergaming as antag roles", "found to have been soft-antag rolling", "made the Janitor a furry against his will",
-        "found to be displaying unhealthy investments in rounds based off of OOC/deadchat commentary", "Mass-RDM. Killed 31 people using electrified grilles",
-        "Openly bragging about being a shitter", "Posting fake porn links isn't funny", "3 bans in 6 months threshold",
-        "factors leading to a lot of attention seeking behavior", "Did nothing but plasmabomb med for 35 minutes",
-        "asking people to do the thug shaker"
+        "Использование модифицированного клиента", "Селф-антаг", "Обход бана", "Датацентр",
+        "Зеркальный бан с nyano", "VAC-бан на secure сервере", "Неприятен в общении на гитхабе и в игре",
+        "В роли CMO участвовал в RP с рабством", "Вышел через секунду после получения ахелпа", "Называл себя Гитлером ямайского джаза",
+        "иди нахуй, больной ублюдок", "ГРУГШТЕЙН", "Альт", "Взорвал сваркой группу секьюрити", "Выгуливал сварочный резервуар",
+        "Рейдер", "Массово убивал людей палкой-потрошителем из подарка", "Постоянный селф-антаг и грифф отдела безопасности",
+        "Подарил вардену моба-курицу, назвав её 'BBC'", "Культист. Можно подать апел через 6 месяцев.", "Буквально имеет 20+ аккаунтов.",
+        "лайкнул трансфобный пост в твиттере", "RP в стиле 'качки избивают (убивают) ботаников (5) и засовывают их в шкафы'",
+        "Персонаж по имени 'АДОЛЬФ РИЗЛЕР'", "написал на бумажке 'я обоссался'", "Полная некомпетентность в роли капитана",
+        "Раздавал AA-ID", "Быть swept", "Быть кармой", "обучал людей изготовлению наркотиков 1-й категории", "RP Гитлера в карго-заказах",
+        "Продажа ядерки", "Намеренно создавал комнату с миазмой", "Выставил AME на 140 и отключился",
+        "Эксплоиты. Телепортировался в оружейку с помощью стульев.", "рекордсмен по speedrun пермабана по правилу 0", "Отдохни, дружок",
+        "Пермабан без права апелляции по рекомендации игроков и хостов", "Метакоммы", "'мягкий антаг ролл', павергейминг, хамство в OOC",
+        "Многократные предупреждения за павергейминг в антаг-ролях", "был уличен в 'мягком антаг ролле'", "сделал уборщика фурри против его воли",
+        "проявлял нездоровую вовлеченность в раунды, согласно OOC/deadchat", "Масс-RDM. Убил 31 человека с помощью электрических решеток",
+        "Открыто хвастался своим мудачеством", "Кидать фейковые порно-ссылки - не смешно", "3 бана за 6 месяцев - порог",
+        "факторы, указывающие на внимание-seeking поведение", "35 минут только и делал, что плазмобомбил медбей",
+        "просил людей станцевать тряску булок"
     ];
 
     private static readonly List<string> Actions =
     [
-        "Pumping nitrous into distro", "Injecting plasma into batteries", "Slipcuffing hos", "Starting a cult", "Rolling for antag",
-        "Plasmaflooding station", "Declaring cargonia", "Evading bans", "Taking fuel tanks for a walk", "Gibbing for no reason",
-        "Bullying trialmins", "ANNOUNCING PRESENCE", "Petting Marsey", "Gossiping about spaceman drama", "Porting /vg/ to 14",
-        "Granting +HOST", "Playing Balalaika", "Anime past 2018 is slop", "Trading Oil", "Abusing Roskomnadzor", "Making fun of people on mastodon",
-        "Hacking the powernet", "Overthrowing the captain", "Building a mech", "Creating AI laws", "Running diagnostics on the engine",
-        "Upgrading the medbay", "Securing the armory", "Repairing hull breaches", "Conducting xenobio research", "Exploring lavaland",
-        "Mining for plasma", "Crafting improv shells", "Setting up the SM", "Breaching into secure areas", "Fighting syndicate operatives",
-        "Escaping on the shuttle", "Reviving dead crewmembers", "Cloning the clown", "Rushing the captain's spare ID", "Sabotaging telecomms",
-        "Disposal bypassing into armory", "Exploiting pulling"
+        "Закачка веселящего газа в дистру", "Заливка плазмы в батареи", "Слип-наручники на ХОСа", "Основание культа", "Ролл на антага",
+        "Плазмозатопление станции", "Провозглашение Каргонии", "Уклонение от банов", "Прогулка с топливными баками", "Гиббинг без причины",
+        "Буллинг триалминов", "ГРОМКО ЗАЯВЛЯЮ О СВОЁМ ПРИСУТСТВИИ", "Гладить Марси", "Сплетни о космических драмках", "Портирование /vg/ на 14",
+        "Выдача +HOST", "Игра на балалайке", "Аниме после 2018 - помойка", "Торговля нефтью", "Злоупотребление Роскомнадзором", "Троллинг людей в мастодоне",
+        "Взлом энергосети", "Свержение капитана", "Сборка меха", "Написание законов ИИ", "Диагностика двигателя",
+        "Апгрейд медбея", "Охрана оружейки", "Ремонт пробоин в корпусе", "Исследования в ксенобио", "Исследование лаваленда",
+        "Добыча плазмы", "Крафт самопальных снарядов", "Настройка СМ", "Прорыв в охраняемые зоны", "Бои с оперативниками Синдиката",
+        "Побег на шаттле", "Воскрешение мёртвых членов экипажа", "Клонирование клоуна", "Захапка запасного ID капитана", "Саботаж телекоммов",
+        "Проникновение в оружейку через мусоропровод", "Эксплуатация механики таскания"
     ];
 
     public string RandomTitle { get; }
